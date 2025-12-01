cf98f44990e929fa533d601609f317ae75525e3a
Augeraa <augeraxel33@gmail.com>
2025-12-01 14:43:57 +0100

    Update octets.c
    3.6

---
+++ TP3/src/octets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void afficher_octets(void *ptr, int taille) {
+    unsigned char *p = ptr;
+    for (int i = 0; i < taille; i++) {
+        printf("%02x ", p[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    short s = 0x0302;
+    int i = 0x04030201;
+    long int l = 0x0807060504030201;
+    float f = 3.14f;
+    double d = 3.14;
+    long double ld = 3.14;
+
+    printf("Octets de short :\n");
+    afficher_octets(&s, sizeof(s));
+
+    printf("Octets de int :\n");
+    afficher_octets(&i, sizeof(i));
+
+    printf("Octets de long int :\n");
+    afficher_octets(&l, sizeof(l));
+
+    printf("Octets de float :\n");
+    afficher_octets(&f, sizeof(f));
+
+    printf("Octets de double :\n");
+    afficher_octets(&d, sizeof(d));
+
+    printf("Octets de long double :\n");
+    afficher_octets(&ld, sizeof(ld));
+
+    return 0;
+}
